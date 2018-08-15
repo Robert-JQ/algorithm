66894c354f9a6b5366e6d72a082a04bf440f4084
jqq <582153160@qq.com>
2018-08-15 17:02:32 +0800

    由于没有找到本书配套jar包可用的maven镜像地址,现将jar包一起上传

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,18 @@
+package base;
+
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.Test;
+
+/**
+ * @author jqq
+ * @desc
+ * @date 2018/08/15
+ * Copyright (c) 2015 jigoon
+ **/
+public class BaseTest {
+
+    @Test
+    public void firstTest() {
+        StdOut.println("Hello World!");
+    }
+}
